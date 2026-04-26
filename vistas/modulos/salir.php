51a1799228ed655b1d416e869235dc563b3487e6
huamanta <alexhe406@gmail.com>
2026-04-26 08:46:35 -0500

    agregado mapa a clientes

--- vistas/modulos/salir.php
+++ vistas/modulos/salir.php
@@ -40,14 +40,7 @@ if (isset($_SESSION['idusuario'])) {
 // Destruir sesión completamente
 $_SESSION = array();
 
-// Eliminar la cookie de sesión si existe
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
-}
+
 
 session_destroy();
 
